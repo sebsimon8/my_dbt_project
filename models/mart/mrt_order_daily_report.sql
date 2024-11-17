f0752d745f2a167bb1e4e13c187465c096f8cad2
Sebastien Simon <sebastiensimon@Sebastiens-MacBook-Air.local>
2024-11-17 19:04:12 +0100

    commit 1

--- models/mart/mrt_order_daily_report.sql
+++ models/mart/mrt_order_daily_report.sql
@@ -1,6 +1,7 @@
 SELECT DATE_TRUNC(order_created_at, DAY) AS reporting_date,
     3 AS column_3,
     2 AS column_2,
+    4 AS column_4,
     mapping.account_manager,
     mapping.state,
     COUNT(DISTINCT order_id) AS total_orders,
