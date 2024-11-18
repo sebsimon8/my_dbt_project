288a0276b9ce04b7f970cb6634429d65ac37f491
Sebastien Simon <sebastiensimon@Sebastiens-MacBook-Air.local>
2024-11-18 21:51:41 +0100

    commit product

--- models/staging/sales_database/stg_sales_database__product.sql
+++ models/staging/sales_database/stg_sales_database__product.sql
@@ -6,5 +6,4 @@ select product_id,
  cast(product_weight_g as int) as product_weight_g,
  cast(product_length_cm as int) as product_length_cm,
  cast(product_height_cm as int) as product_height_cm,
- cast(product_width_cm as int) as product_width_cm
 from {{ source('sales_database', 'product') }}
