12d913a71ccfe9197b414bb969cc940df5669a21
Sebastien Simon <sebastiensimon@Sebastiens-MacBook-Air.local>
2024-11-18 21:51:07 +0100

    commit feedback

--- models/staging/sales_database/stg_sales_database__feedback.sql
+++ models/staging/sales_database/stg_sales_database__feedback.sql
@@ -3,5 +3,4 @@ select
  order_id,
  feedback_score,
  DATETIME(feedback_form_sent_date, "Europe/Paris") AS feedback_form_sent_at,
- DATETIME(feedback_answer_date, "Europe/Paris") AS feedback_answered_at,
 from {{ source('sales_database', 'feedback') }}
